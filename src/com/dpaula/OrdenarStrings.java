8591d1715d385f9790cd7564f801bdbc3905818f
dpaula <fernando.dpaula@gmail.com>
2018-03-23 23:53:17 -0300

    projeto base eclipse java

---
+++ src/com/dpaula/OrdenarStrings.java
@@ -0,0 +1,51 @@
+package com.dpaula;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class OrdenarStrings {
+
+	public static void main(String[] args) {
+
+		List<String> nomes = Arrays.asList("Fernando de Paula de Lima", "Carla Weitzel", "Carmem Lucia", "Pamela");
+
+		// Collections.sort(nomes, new NomesSort());
+
+		nomes.sort(new NomesSort());
+
+		System.out.println(nomes);
+
+		nomes.forEach(new Consumidor());
+
+	}
+
+}
+
+class Consumidor implements Consumer<String> {
+
+	@Override
+	public void accept(String t) {
+		System.out.println(t);
+	}
+
+}
+
+class NomesSort implements Comparator<String> {
+
+	@Override
+	public int compare(String s1, String s2) {
+
+		if (s1.length() < s2.length()) {
+			return -1;
+		}
+
+		if (s1.length() > s2.length()) {
+			return 2;
+		}
+
+		return 0;
+	}
+
+}
