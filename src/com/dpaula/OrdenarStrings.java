c2e1a4c74c9696ec0129237c35dc6746ac0c3346
dpaula <fernando.dpaula@gmail.com>
2018-03-24 17:31:11 -0300

    lambda, entendendo melhor o uso, a conversao de uma funcao para uma interface funcional

--- src/com/dpaula/OrdenarStrings.java
+++ src/com/dpaula/OrdenarStrings.java
@@ -12,6 +12,27 @@ public class OrdenarStrings {
 
 		nomes.sort(new NomesSort());
 
+		// lambda
+		// pega esta função como parametro e transforma em uma interface funcional
+		// neste caso no método campare da interface Comparator
+		nomes.sort((s1, s2) -> {
+
+			if (s1.length() < s2.length()) {
+				return -1;
+			}
+
+			if (s1.length() > s2.length()) {
+				return 2;
+			}
+
+			return 0;
+		});
+
+		// utilizando metodo compare da api Integer que compara dois valores, e rotorna
+		// 1, -1 e 0
+		// basta passar para lambda que vai entender e converter em um comparator
+		nomes.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
 		System.out.println(nomes);
 
 		nomes.forEach(s -> System.out.println(s));
