ce321e3f99b47994d8bb2e3b7d90bd4d557a971b
dpaula <fernando.dpaula@gmail.com>
2018-03-24 22:38:54 -0300

    trabalhando com retornos do tipo Optional

---
+++ src/com/dpaula/ExemplosOptional.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.dpaula;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+
+/**
+ * @author ferna
+ *
+ */
+public class ExemplosOptional {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		List<Curso> cursos = Arrays.asList(new Curso("Python", 45), new Curso("JavaScript", 150),
+				new Curso("Java 8", 113), new Curso("C", 55));
+
+		cursos.stream()/**/
+				.filter(c -> c.getAlunos() > 100)/**/
+				.findAny()/**/// busca qualquer um
+				.ifPresent(c -> System.out.println(c.getNome()));/**/// como retorna um Optional, se o resultado não for
+																		// nulo, executa o lambda
+
+		System.out.println();
+
+		// traz um optionalDouble
+		OptionalDouble media = cursos.stream()/**/
+				.filter(c -> c.getAlunos() > 1000)/**/
+				.mapToDouble(c -> c.getAlunos())/**/// mapeia e traz uma lista de numeros
+				.average();/**/// traz media
+
+		System.out.println(media.orElse(0));
+	}
+
+}
